341942814e508db70c11042502bf8ca2bab93755
minodisk <daisuke.mino@gmail.com>
2017-07-24 15:53:50 +0900

    Run test with JSDom

--- test/link.tsx
+++ test/link.tsx
@@ -1,1 +1,23 @@
+import {mount} from "enzyme";
+import "jsdom-global/register";
 import "mocha";
+import {strictEqual} from "power-assert";
+import * as React from "react";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+
+import {Link} from "../lib";
+
+describe("<Link>", () => {
+
+  describe("to", () => {
+
+    it("should be href",  () => {
+      const mockStore = configureStore();
+      const wrapper = mount(<Provider store={mockStore()}><Link to="/users" /></Provider>);
+      strictEqual(wrapper.find("a").length, 1);
+    });
+
+  });
+
+});
